024eb0f782e1c1793f1417ba0d64e8eb10534f13
zandirrr <146164988+zandirrr@users.noreply.github.com>
2023-11-13 22:06:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,54 +1,82 @@
 #include <iostream>
-#include <windows.h>
-#include <cstdlib>
 
 using namespace std;
 
-string imie;
+int main(){
+
+int pkt = 0;
 string odp;
-
-int main()
-{
-    //https://cpp0x.pl/kursy/Kurs-WinAPI-C++/Roznosci/Kolory-w-konsoli/374
-    HANDLE uchwyt = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(uchwyt, 12);
-    cout << "\aPodaj imie: ";
-    cin >> imie;
-    int pkt = 0;
-
-    system("cls");
-    cout<< "3";
-    Sleep(1000);
-    system("cls");
-    cout<< "3..2";
-    Sleep(1000);
-    system("cls");
-    cout<< "3..2..1..";
-    Sleep(1000);
-    system("cls");
-
-    cout << imie << " witaj w tescie z programowania\n";
-
-    cout << "Co to jest C++?"<<endl;
-    cout << "a) Piosenkarka"<<endl;
-    cout << "b) Jezyk programowania"<<endl;
-    cout << "c) Mlynek do herbaty"<<endl;
-    cout << "Twoja odpowiedz: ";
+    cout<<"Kto był pierwszym prezydentem Stanów Zjednoczonych?"<<endl;
+    cout<<"a)George Washington"<<endl;
+    cout<<"b)John Adams"<<endl;
+    cout<<"c)Thomas Jefferson"<<endl;
+    cout<<"d)James Madison"<<endl;
     cin >> odp;
-    if(odp == "b")
-    {
-        cout << "Poprawna odpowiedz!";
-        pkt++;
-    }
-    else
-    {
-        cout << "Zle! Jest to jezyk programowania!";
+        if(odp == "a"){
+                cout<<"To jest poprawna odpowiedz"<<endl;
+            pkt +=1;
+        }
+        else{
+            cout<<"to nie jest dobra odpowiedz poprawna odpowiedza byla odpowiedz: a"<<endl;
+        }
 
-    }
-    Sleep(3000);
-    system("cls");
+    cout<<"Najwieksze miasto swiata to?"<<endl;
+    cout<<"a)Buenos Aires"<<endl;
+    cout<<"b)Mexico City"<<endl;
+    cout<<"c)Nowy Jork "<<endl;
+    cout<<"d)Tokio"<<endl;
+    cin >> odp;
+        if(odp == "d"){
+                cout<<"To jest poprawna odpowiedz"<<endl;
+            pkt +=1;
+        }
+        else{
+            cout<<"to nie jest dobra odpowiedz poprawna odpowiedza byla odpowiedz: d"<<endl;
+        }
+        cout<<"Najwyzszy szczyt swiata to?"<<endl;
+    cout<<"a)Dhaulagiri"<<endl;
+    cout<<"b)K2"<<endl;
+    cout<<"c)Mount Everest"<<endl;
+    cout<<"d)Makalu"<<endl;
+    cin >> odp;
+        if(odp == "c"){
+                cout<<"To jest poprawna odpowiedz"<<endl;
+            pkt +=1;
+        }
+        else{
+            cout<<"to nie jest dobra odpowiedz poprawna odpowiedza byla odpowiedz: d"<<endl;
+        }
+
+    cout<<"najwiekszy kontynent to?"<<endl;
+    cout<<"a)Europa"<<endl;
+    cout<<"b)Azja"<<endl;
+    cout<<"c)Afryka"<<endl;
+    cout<<"d)Ameryka Połnocna"<<endl;
+    cin >> odp;
+        if(odp == "b"){
+                cout<<"To jest dobra odpowiedz"<<endl;
+            pkt +=1;
+        }
+        else{
+            cout<<"to nie jest dobra odpowiedz poprawna odpowiedza byla odpowiedz: d"<<endl;
+        }
+
+    cout<<"Kto jest zalozycielem Amazona?"<<endl;
+    cout<<"a)Jeff Bezos"<<endl;
+    cout<<"b)Elon Musk"<<endl;
+    cout<<"c)Bill Gates"<<endl;
+    cout<<"d)Steve Wozniak"<<endl;
+    cin >> odp;
+        if(odp == "a"){
+                cout<<"To jest dobra odpowiedz"<<endl;
+            pkt +=1;
+        }
+        else{
+            cout<<"to nie jest dobra odpowiedz poprawna odpowiedza byla odpowiedz: d"<<endl;
+        }
 
 
+cout<<"zebrales: "<<pkt<<" pkt"<<endl;
 
     return 0;
 }
